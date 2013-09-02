f7c944f77a980d146d8442f9953b61889d6026fa
seanherron <seanherron@gmail.com>
2013-09-01 21:50:41 -0700

    trimmed

--- scripts/createadmin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-if User.objects.count() == 0:
-    admin = User.objects.create_user('sean', 'seanherron@gmail.com', 'password123456')
-    admin.is_superuser = True
-    admin.is_staff = True
-    admin.save()
