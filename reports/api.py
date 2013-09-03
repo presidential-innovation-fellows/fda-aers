412119558dd2560319cefdb0d6908185874c0797
seanherron <seanherron@gmail.com>
2013-09-03 17:22:22 -0400

    all da filters

--- reports/api.py
+++ reports/api.py
@@ -3,6 +3,13 @@ from tastypie.resources import Resource, ALL, ALL_WITH_RELATIONS
 from tastypie.resources import ModelResource
 from .models import Report, Drug, Reaction, ReportDuplicate
 
+
+
+numeric_filtering = ['exact', 'iexact', 'contains', 'icontains', 'in', 'startswith', 'istartswith', 'endswith' , 'iendswith', 'isnull', 'range', 'search', 'regex', 'iregex']
+text_filtering = ['exact', 'iexact', 'contains', 'icontains', 'in', 'startswith', 'istartswith', 'endswith' , 'iendswith', 'isnull', 'search', 'regex', 'iregex']
+date_filtering = ['year', 'month', 'day', 'week_day', 'isnull', 'exact', 'in', 'gt', 'gte', 'lt', 'lte']
+boolean_filtering = ['exact', 'iexact', 'isnull']
+
 class ReportResource(ModelResource):
     drugs = fields.ToManyField('reports.api.DrugResource', 'drug_set', full=True, null=True)
     reportduplicates = fields.ToManyField('reports.api.ReportDuplicateResource', 'reportduplicate_set', full=True, null=True)
@@ -14,8 +21,36 @@ class ReportResource(ModelResource):
         fields = ['safetyreportid', 'safetyreportversion', 'primarysourcecountry', 'occurcountry', 'transmissiondate', 'reporttype', 'serious', 'seriousnessdeath', 'seriousnesslifethreatening', 'seriousnesshospitalization', 'seriousnessdisabling', 'seriousnesscongenitalanomali', 'seriousnessother', 'receivedateformat', 'receivedate', 'recieptdateformat', 'recieptdate', 'fullfillexpeditecriteria', 'companynumb', 'reportercountry', 'duplicate', 'qualification', 'sendertype', 'senderorganization', 'receivertype', 'receiverorganization', 'patientonsetage', 'patientonsetageunit', 'patientweight', 'patientsex'] 
         collection_name = "safetyreports"
         filtering = {
-            'companynumb': ['exact', 'iexact', 'contains', 'icontains', 'in', 'startswith', 'istartswith', 'endswith' , 'iendswith', 'search', 'regex', 'iregex'],
-            'safetyreportid': ['exact', 'iexact', 'contains', 'icontains', 'in', 'startswith', 'istartswith', 'endswith' , 'iendswith', 'range', 'search', 'regex', 'iregex'],
+            'safetyreportid': numeric_filtering,
+            'safetyreportversion': numeric_filtering,
+            'primarysourcecountry': text_filtering,
+            'occurcountry': text_filtering,
+            'transmissiondate': date_filtering,
+            'reporttype': numeric_filtering,
+            'serious': boolean_filtering,
+            'seriousnessdeath': boolean_filtering,
+            'seriousnesslifethreatening': boolean_filtering,
+            'seriousnesshospitalization': boolean_filtering,
+            'seriousnessdisabling': boolean_filtering,
+            'seriousnesscongenitalanomali': boolean_filtering,
+            'seriousnessother': boolean_filtering,
+            'receivedateformat': text_filtering,
+            'receivedate': date_filtering,
+            'recieptdateformat': text_filtering,
+            'recieptdate': date_filtering,
+            'fullfillexpeditecriteria': boolean_filtering,
+            'companynumb': text_filtering,
+            'reportercountry': text_filtering,
+            'duplicate': boolean_filtering,
+            'qualification': numeric_filtering,
+            'sendertype': numeric_filtering,
+            'senderorganization': text_filtering,
+            'receivertype': numeric_filtering,
+            'receiverorganization': text_filtering,
+            'patientonsetage': numeric_filtering,
+            'patientonsetageunit': numeric_filtering,
+            'patientweight': numeric_filtering,
+            'patientsex': numeric_filtering,
         }
 
 class ReportDuplicateResource(ModelResource):    
